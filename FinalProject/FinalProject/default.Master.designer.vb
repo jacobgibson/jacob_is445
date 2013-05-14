3e195cb28a60352af7a981342ed6390722288bc0
Jacob Gibson <jacobgbsn@gmail.com>
2013-05-13 17:58:48 -0700

    finalprojectv3

--- FinalProject/FinalProject/default.Master.designer.vb
+++ FinalProject/FinalProject/default.Master.designer.vb
@@ -32,15 +32,6 @@ Partial Public Class _default
     Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
 
     '''<summary>
-    '''Menu1 control.
-    '''</summary>
-    '''<remarks>
-    '''Auto-generated field.
-    '''To modify move field declaration from designer file to code-behind file.
-    '''</remarks>
-    Protected WithEvents Menu1 As Global.System.Web.UI.WebControls.Menu
-
-    '''<summary>
     '''SiteMapDataSource1 control.
     '''</summary>
     '''<remarks>
@@ -50,13 +41,13 @@ Partial Public Class _default
     Protected WithEvents SiteMapDataSource1 As Global.System.Web.UI.WebControls.SiteMapDataSource
 
     '''<summary>
-    '''SiteMapPath1 control.
+    '''Menu1 control.
     '''</summary>
     '''<remarks>
     '''Auto-generated field.
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
-    Protected WithEvents SiteMapPath1 As Global.System.Web.UI.WebControls.SiteMapPath
+    Protected WithEvents Menu1 As Global.System.Web.UI.WebControls.Menu
 
     '''<summary>
     '''TreeView1 control.
@@ -66,6 +57,15 @@ Partial Public Class _default
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents TreeView1 As Global.System.Web.UI.WebControls.TreeView
+
+    '''<summary>
+    '''SiteMapPath1 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents SiteMapPath1 As Global.System.Web.UI.WebControls.SiteMapPath
 
     '''<summary>
     '''ContentPlaceHolder1 control.
